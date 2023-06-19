8582de3809d65251c7f7862cf1a77ae248c3f53b
DomenicoCoppeta <domenicocoppeta@gmail.com>
2023-06-19 21:04:47 +0200

    lezione1

---
+++ Index.js
@@ -0,0 +1,10 @@
+let zuccheroBianco = 550
+let farina = 120
+let lievito = 1000000000
+let ciotolaMedia = zuccheroBianco + farina + lievito
+
+// ciotolaMedia = ciotolaMedia + 100 può essere scritto come nella riga sottostante
+
+ciotolaMedia += 100
+
+console.log(ciotolaMedia)
