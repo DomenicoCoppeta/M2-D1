d48b5a4b4f447700aa14d85cdee6d38b378c9cd3
DomenicoCoppeta <domenicocoppeta@gmail.com>
2023-06-21 22:41:50 +0200

    21/06

--- Index.js
+++ Index.js
@@ -1,10 +1,10 @@
-let zuccheroBianco = 550
-let farina = 120
-let lievito = 1000000000
-let ciotolaMedia = zuccheroBianco + farina + lievito
+// Definizione Algoritmo:
+// Un'algoritmo è una sequenza di calcoli o operazioni logiche creata per elaborare un input ed ottenere un determinato output.
+// L'esempio più comune è quello di paragonare un'algoritmo ad una ricetta di cucina, la quale parte da determinati ingredienti (input) per ottenere la pietanza finita (output) tramite la sua esecuzione (algoritmo).
 
-// ciotolaMedia = ciotolaMedia + 100 può essere scritto come nella riga sottostante
+// Definizione Variabile:
+// Le variabili sono allocazioni di memoria in cui l'algorimto inserisce dati che hanno bisogno di una elaborazione intermedia, prima di poter essere utilizzati per completare il processo.
+// Sempre utilizzando la ricetta culinaria come esempio, una variabile equivale ad un contenitore, in cui andremo a mettere ingredienti che hanno bisgno di una lavorazione intermedia per poter essere utilizzati, come potrebbero essere uova e farina per crare l'impasto base della ricetta.
 
-ciotolaMedia += 100
-
-console.log(ciotolaMedia)
+// Undefined vs. Null:
+// Quando una viariabile viene dichiarata non ha un contenuto definito (undefined), mentre null è la keyword che svuota il contenuto di una variabile.
